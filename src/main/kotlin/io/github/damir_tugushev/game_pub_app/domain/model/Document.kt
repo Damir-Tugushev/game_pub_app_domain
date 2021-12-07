5b0994ca377f2425c9818fbd07b791775b960ad1
Damir-Tugushev <0damir.1tugushev@gmail.com>
2021-12-07 12:38:55 +0300

    Replacing Blob with string Uri

--- src/main/kotlin/io/github/damir_tugushev/game_pub_app/domain/model/Document.kt
+++ src/main/kotlin/io/github/damir_tugushev/game_pub_app/domain/model/Document.kt
@@ -1,9 +1,8 @@
 package io.github.damir_tugushev.game_pub_app.domain.model
 
-import java.sql.Blob
 import java.sql.Timestamp
 
 public interface Document : Entity<Int> {
-    public val content: Blob?
+    public val contentUri: String
     public val creationDate: Timestamp
 }
